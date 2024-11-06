487d86210a8598a973bf2ca1fd36b11bf607c774
mehmetadiyaman <mn.adymn23@gmail.com>
2024-11-06 18:43:14 +0300

    veriler gösterilmeye çalışılıyor

--- src/controllers/current_news_controller.js
+++ src/controllers/current_news_controller.js
@@ -1,21 +1,38 @@
-// controllers/current_news_controller.js
+/**
+ * @license MIT
+ * @copyright 2024 EMMİ
+ */
 
 "use strict";
 
+/**
+ * Özel modüller
+ */
 const Guncel = require("../models/guncel_model");
+const getPagination = require("../utils/get_pagination_util");
 
 /**
- * Güncel haberleri göster
+ * Güncel haberleri göster ve sayfalama yap
  * @param {object} req
  * @param {object} res
  * @throws {Error}
  */
+
 const renderCurrentNews = async (req, res) => {
   try {
-    const news = await Guncel.find().sort({ published: "desc" }); // Yayınlanma tarihine göre sırala
+    const totalNews = await Guncel.countDocuments();
+    const pagination = getPagination("/currentnews", req.query, 10, totalNews); // Current news için "/currentnews?page=1"
+
+    const latestNews = await Guncel.find()
+      .select("title link published summary")
+      .sort({ published: "desc" })
+      .limit(pagination.limit)
+      .skip(pagination.skip);
+
     res.render("./pages/currentnews", {
       sessionUser: req.session.user,
-      news,
+      articles: latestNews,
+      pagination,
     });
   } catch (error) {
     console.error("Güncel haberleri çekerken bir hata oluştu:", error.message);
